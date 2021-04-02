39bb240f8d5396a81d085a797cba0e044692ce80
moyses-santos <moyzlevi@gmail.com>
2021-04-02 17:36:22 -0300

    Implemented findAll in SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,15 +16,25 @@ public class Program {
 		
 		System.out.println("~~~~Test1: seller findById ~~~~");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		System.out.println();
+		
 		System.out.println("~~~~Test2: seller findByDepartment ~~~~");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj :list) {
 			System.out.println(obj);
 		}
+		System.out.println();
+		
+		System.out.println("~~~~Test2: seller findAll ~~~~");
+		list = sellerDao.findAll();
+		for(Seller obj :list) {
+			System.out.println(obj);
+		}
+		System.out.println();
+		
+		
 	}
 
 }
