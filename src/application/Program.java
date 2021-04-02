9a4fa62ea0408de22ee23f9efbb0e487130113a4
moyses-santos <moyzlevi@gmail.com>
2021-04-02 18:47:43 -0300

    Implemented update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,14 +35,17 @@ public class Program {
 		}
 		System.out.println();
 		
-
 		System.out.println("~~~~Test4: seller insert ~~~~");
 		Seller newSeller = new Seller(null, "Greg","greg@gmail.com",new Date(),4000.0, department);
 		sellerDao.insert(newSeller);
 		
 		System.out.println("Inserted! New Id: "+ newSeller.getId());
 		
+		System.out.println("~~~~Test5: seller insert ~~~~");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
 		
+		System.out.println("Update completed");
 	}
-
 }
