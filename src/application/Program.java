015922cbf5dced5dfc85f645c8d26ceb505ba1c6
moyses-santos <moyzlevi@gmail.com>
2021-04-02 18:55:01 -0300

    Implemented delete in sellerJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,17 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
-import util.Util;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("~~~~Test1: seller findById ~~~~");
@@ -47,5 +47,13 @@ public class Program {
 		sellerDao.update(seller);
 		
 		System.out.println("Update completed");
+		
+		System.out.println("~~~~Test6: seller delete ~~~~");
+		System.out.println("Enter a digit for a delete test:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deleted Completed!");
+		
+		
 	}
 }
